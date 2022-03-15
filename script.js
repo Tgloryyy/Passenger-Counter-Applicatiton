a53f36f89861de2ef5f514d73629951914ad91ca
Tgloryyy <Gloriousthapa2@gmail.com>
2022-03-15 11:19:52 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+
+let c=0
+
+function increment(){
+    c=c+1
+    document.getElementById("count").innerText=c
+}
+
+function save(){
+    let savestr=c + " "
+    document.getElementById("save").textContent+=savestr
+    
+    c=0
+    document.getElementById("count").innerText=0
+}
+
